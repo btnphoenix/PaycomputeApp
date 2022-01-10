650bf57dcc027a73888458185fb880e84556192c
allamanenihemanth <allamaneni.hemanth@outlook.com>
2022-01-09 19:02:46 -0500

    Create and commit to some controller functionality

--- Paycompute.Entity/Employee.cs
+++ Paycompute.Entity/Employee.cs
@@ -9,22 +9,27 @@ namespace Paycompute.Entity
 {
     public class Employee
     {
+        public string FullName { get; set; }
+        [Required, MaxLength(100)]
+
         public int Id { get; set; } 
         [Required]
         public string EmployeeNo { get; set; }
         [Required, MaxLength(50)]
         public string FirstName { get; set; }
         [Required, MaxLength(50)]
+        public string MiddleName { get; set; }
         public string LastName { get; set; }
         [Required, MaxLength(50)]
         public string Gender { get; set; }  
-        public string ImageURL { get; set; }    
+        public string ImageUrl { get; set; }    
         public DateTime DOB { get; set; }
-        public DateTime DateJoined { get; set; }    
+        public DateTime DateJoined { get; set; }  
+        public string Phone { get; set; }
         public string Designation { get; set; }
         public string Email { get; set; }
         [Required, MaxLength(50)]
-        public string SocialSeurityNo { get; set; }
+        public string SocialSecurityNo { get; set; }
         public PaymentMethod PaymentMethod { get; set; }
         public StudentLoan StudentLoan { get; set; }
         public UnionMember UnionMember { get; set; }
---
+++ Paycompute.Persistence/Migrations/20220109231154_UpdatedEmployee.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Paycompute.Persistence.Migrations
+{
+    public partial class UpdatedEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageURL",
+                table: "Employees",
+                newName: "ImageUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "SocialSeurityNo",
+                table: "Employees",
+                newName: "SocialSecurityNo");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MiddleName",
+                table: "Employees",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "MiddleName",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Employees",
+                newName: "ImageURL");
+
+            migrationBuilder.RenameColumn(
+                name: "SocialSecurityNo",
+                table: "Employees",
+                newName: "SocialSeurityNo");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
